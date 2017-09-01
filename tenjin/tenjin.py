1e865c7c5d8ec01116a2127ae696eec425a23f94
Rohan Mahajan <you@example.com>
2017-09-01 12:06:19 -0700

    script and tenjin

--- tenjin/tenjin.py
+++ tenjin/tenjin.py
@@ -1,11 +1,34 @@
+TEMPLATE_PREFIX = "::"
+TEMPLATE_PREFIX_LEN = len(TEMPLATE_PREFIX)
+
 def read_file(filename):
    with open(filename, "rb") as fp:
-    bytes = fp.read()
+    bytes = fp.readlines()
     return bytes
 
-def run_object():
-   print 5 
+
+def analyze_template_synthax(line, stack):
+    print "entering template synthax"
+    new_line = "print \"" + line[TEMPLATE_PREFIX_LEN:-1] +"\"" + line[-1]    
+    return new_line
+def analyze_python_synthax(line, stack):
+    print "entering answering python synthax"
+    newline = line
+    return newline
+def transform_template_lines(script_lines):
+   stack = []
+   for i in range(len(script_lines)):
+       line = script_lines[i]
+       if line.startswith(TEMPLATE_PREFIX):
+         new_line = analyze_template_synthax(line, stack)
+       else:
+	 new_line = analyze_python_synthax(line, stack)
+       script_lines[i] = new_line	
+   return script_lines
 
 def run(filename, variables):
-   script = read_file(filename)
+   template_lines = read_file(filename)
+   script_lines = transform_template_lines(template_lines)
+   script = "".join(script_lines)
+   print script_lines
    exec(script, variables)
