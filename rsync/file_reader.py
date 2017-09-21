0aaf52b7a2b1f8652f1ce0b3fb51ac28ec50328a
Rohan Mahajan <rohanmahajan1993@gmail.com>
2017-09-20 18:18:09 -0700

    more changes for filereader

--- rsync/file_reader.py
+++ rsync/file_reader.py
@@ -5,18 +5,27 @@ import time
 import zlib
 
 BLOCK_SIZE = 8192
+
+"""
+Wrapper for creating the complicated hash.
+We choose to use sha1, but this can easily swapped out.
+"""
 def generateComplicatedHash(bytes):
     sha_object = hashlib.sha1()
     complicatedHashObject = sha_object.update(bytes)
     complicatedHash = sha_object.digest()
     return complicatedHash
 
+"""
+Wrapper for reading file
+"""
 def file_read(filename):
   with open(filename, "rb") as fp:
     bytes = fp.read()
     return bytes
-
+"""
+Wrapper for file friting
+"""
 def file_write(filename, bytes):
   with open(filename, "wb") as fp:
      fp.write(bytes)
-
