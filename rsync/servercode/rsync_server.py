58096cf496075c81a9e9f98597d8323498420d2b
Rohan Mahajan <rohanmahajan1993@gmail.com>
2017-09-20 17:22:32 -0700

    creating more documentation

--- rsync/servercode/rsync_server.py
+++ rsync/servercode/rsync_server.py
@@ -7,21 +7,24 @@ import time
 import protocols_pb2_grpc
 import server_helper
 
-
 from concurrent import futures
 
+
 class Rsync(protocols_pb2_grpc.RsyncServiceServicer):
-   def RsyncMethod(self, request, context):
-     return server_helper.process_server_directory(request)
+    def RsyncMethod(self, request, context):
+        return server_helper.process_server_directory(request)
+
 
 def serve():
-  server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-  protocols_pb2_grpc.add_RsyncServiceServicer_to_server(Rsync(), server)
-  server.add_insecure_port('[::]:50051')
-  server.start()
-  try:
-    while True:
-      time.sleep(10000)
-  except KeyboardInterrupt:
-    server.stop(0)
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    protocols_pb2_grpc.add_RsyncServiceServicer_to_server(Rsync(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    try:
+        while True:
+            time.sleep(10000)
+    except KeyboardInterrupt:
+        server.stop(0)
+
+
 serve()
--- rsync/servercode/server_helper.py
+++ rsync/servercode/server_helper.py
@@ -1,30 +1,44 @@
+import file_reader
+import protocols_pb2
+
 import hashlib
 import os
-import protocols_pb2
 import time
 import zlib
-import file_reader
 
-BLOCK_SIZE = 8192
+"""
+This library handles most of the core logic for the rsync server.
+"""
 
-# Will see if files still exists and need to be deleted
-# Find out addition of 
+"""
+This library is responsible for creating the edited and deleted files.
+If a file is found in the client and not in the server, we assume that
+the file has to be deleted. If a file is found in both, we analyze it to be deleted.
+Some tricky situations involve a directory being deleted and new a file with the same being
+created and vice-versa.
+"""
 def analyze_client_files(timestamp, clientFiles, deletedFiles, editedFiles):
     clientFileNames = dict()
     for clientFile in clientFiles:
         filename = clientFile.file.filename
         clientFileNames[filename] = clientFile.file.isDirectory
-        if os.path.exists(filename) and os.path.isdir(filename) == clientFile.file.isDirectory:
-           print "the filename is", filename
-           last_modified_time = os.path.getmtime(filename)  
-           if last_modified_time > timestamp and not clientFile.file.isDirectory:
-               analyze_client_file(clientFile, editedFiles)
+        if os.path.exists(filename) and os.path.isdir(
+                filename) == clientFile.file.isDirectory:
+            print "the filename is", filename
+            last_modified_time = os.path.getmtime(filename)
+            if last_modified_time > timestamp and not clientFile.file.isDirectory:
+                analyze_client_file(clientFile, editedFiles)
         else:
             deleted_file = deletedFiles.add()
             deleted_file.isDirectory = clientFile.file.isDirectory
             deleted_file.filename = filename
     return clientFileNames
 
+"""
+This method is used to create a hash map
+which maps the clientSimpleHashes to their block number and complicatedHash.
+This format is used to generate the diffs between files.
+"""
 
 def create_client_hashmap(clientFile):
     clientHashMap = dict()
@@ -35,7 +49,21 @@ def create_client_hashmap(clientFile):
         blockNumber += 1
     return clientHashMap
 
-def fileEditHelper(editedFiles, fileEdits, isBlockNumber, fileName, blockNumber=0, fileContent="", numBlocks=0):
+"""
+This method first allows an easy method to create the editedFile protobuf.
+Protobufs can't be passed parameters in their creation, so this helps reduce
+verbosity. Additionally, this method adds a check to creat and editedFile if it 
+is not created before. This awkard structure is to ensure we don't have editedFiles,
+which consist of zero changes.
+"""
+
+def fileEditHelper(editedFiles,
+                   fileEdits,
+                   isBlockNumber,
+                   fileName,
+                   blockNumber=0,
+                   fileContent="",
+                   numBlocks=0):
     if fileEdits == None:
         editedFile = editedFiles.add()
         editedFile.fileName = fileName
@@ -49,72 +77,113 @@ def fileEditHelper(editedFiles, fileEdits, isBlockNumber, fileName, blockNumber=
         fileEdit.fileContent = fileContent
     return fileEdits
 
+"""
+This method is responsible for creating the diff between the clientFile and 
+the serverFile. We use the same algorithm that rysnc uses. We first compare the
+simple checksum and see if matches. We then see if a more complicated checksum matches.
+We assume that if two complicated checksums match, the blocks are equivalent.
+We try to ensure conesecutive changes of the same kind are grouped together in the fileEdit structure.
+For instance, instead of having two fileEdits that both contain raw bytes, we instead one raw byte structure.
+"""
 def analyze_client_file(clientFile, editedFiles):
     fileEdits = None
     clientHashMap = create_client_hashmap(clientFile)
     print clientFile.file.filename
     with open(clientFile.file.filename, "rb") as fp:
-        new_bytes = fp.read(BLOCK_SIZE)
+        new_bytes = fp.read(file_reader.BLOCK_SIZE)
         current_bytes = ""
         numBlocks = 0
         previousBlock = -5
         while new_bytes != "":
-           simpleHash = zlib.adler32(new_bytes) 
-           foundBlockMatch = False
-           if simpleHash in clientHashMap:
-              complicatedServerHash = file_reader.generateComplicatedHash(bytes)
-              hashList = clientHashMap[simpleHash]
-              for blockNumber, complicatedHash in hashList:
-                if complicatedHash == complicatedServerHash:
-                    foundBlockMatch = True
-                    new_bytes = fp.read(BLOCK_SIZE) 
-                    if numBlocks == 0 and current_bytes != "":
-                        fileEdits = fileEditHelper(editedFiles, fileEdits, clientFile.file.fileName, False, fileContent=current_bytes)
-                        current_bytes = ""
-                    elif blockNumber - numBlocks == previousBlock:
-                        numBlocks += 1
-                    else:
-                        fileEdits = fileEditHelper(editedFiles, fileEdits, clientFile.file.fileName, True, blockNumber=previousBlock, numBlocks=numBlocks)
-                        numBlocks = 1
-                        previousBlock = blockNumber
-           if not foundBlockMatch:
-               if numBlocks != 0:
-                  fileEdits = fileEditHelper(editedFiles, fileEdits, clientFile.file.fileName, True, blockNumber=previousBlock, numBlocks=numBlocks)
-                  numBlocks = 0
-                  current_bytes += new_bytes[0] 
-               new_bytes = new_bytes[1:] + fp.read(1)
+            simpleHash = zlib.adler32(new_bytes)
+            foundBlockMatch = False
+            if simpleHash in clientHashMap:
+                complicatedServerHash = file_reader.generateComplicatedHash(
+                    bytes)
+                hashList = clientHashMap[simpleHash]
+                for blockNumber, complicatedHash in hashList:
+                    if complicatedHash == complicatedServerHash:
+                        foundBlockMatch = True
+                        new_bytes = fp.read(file_reader.BLOCK_SIZE)
+                        if numBlocks == 0 and current_bytes != "":
+                            fileEdits = fileEditHelper(
+                                editedFiles,
+                                fileEdits,
+                                clientFile.file.fileName,
+                                False,
+                                fileContent=current_bytes)
+                            current_bytes = ""
+                        elif blockNumber - numBlocks == previousBlock:
+                            numBlocks += 1
+                        else:
+                            fileEdits = fileEditHelper(
+                                editedFiles,
+                                fileEdits,
+                                clientFile.file.fileName,
+                                True,
+                                blockNumber=previousBlock,
+                                numBlocks=numBlocks)
+                            numBlocks = 1
+                            previousBlock = blockNumber
+            if not foundBlockMatch:
+                if numBlocks != 0:
+                    fileEdits = fileEditHelper(
+                        editedFiles,
+                        fileEdits,
+                        clientFile.file.fileName,
+                        True,
+                        blockNumber=previousBlock,
+                        numBlocks=numBlocks)
+                    numBlocks = 0
+                    current_bytes += new_bytes[0]
+                new_bytes = new_bytes[1:] + fp.read(1)
         if numBlocks != 0:
-            fileEdits = fileEditHelper(editedFiles, fileEdits, clientFile.file.fileName, True, blockNumber=previousBlock, numBlocks=numBlocks)
+            fileEdits = fileEditHelper(
+                editedFiles,
+                fileEdits,
+                clientFile.file.fileName,
+                True,
+                blockNumber=previousBlock,
+                numBlocks=numBlocks)
         elif current_bytes != "":
-            fileEdits = fileEditHelper(editedFiles, fileEdits, clientFile.file.fileName, False, fileContent=current_bytes)
-
-
-#Have to handle weird case where file is deleted and directory is created with same name
-#Just find new files
-def analyze_server_files(basefile, clientFileNames, new_files):
-    tuples = os.walk(basefile) 
+            fileEdits = fileEditHelper(
+                editedFiles,
+                fileEdits,
+                clientFile.file.fileName,
+                False,
+                fileContent=current_bytes)
+"""
+This method find all the new files, files that exist on the server
+but not on the client. Once again this is made tricky by cases such
+as deleting a directory and creating a file with the same name.
+"""
+def find_new_files(basefile, clientFileNames, new_files):
+    tuples = os.walk(basefile)
     print clientFileNames
-    for directory, _, filenames in tuples: 
-       if directory not in clientFileNames or clientFileNames[directory] != True:
-           new_file = new_files.add()
-           new_file.file.filename = directory
-           new_file.file.isDirectory = True
-       for filename in filenames:        
-          full_name = os.path.join(directory, filename)  
-          if not os.path.isdir(full_name):
-              if full_name not in clientFileNames or clientFileNames[full_name]:
-                new_file = new_files.add()
-                new_file.file.filename = full_name
-                new_file.file.isDirectory = False
-                file_content = file_reader.file_read(full_name)
-                new_file.fileContent = file_content
+    for directory, _, filenames in tuples:
+        if directory not in clientFileNames or clientFileNames[directory] != True:
+            new_file = new_files.add()
+            new_file.file.filename = directory
+            new_file.file.isDirectory = True
+        for filename in filenames:
+            full_name = os.path.join(directory, filename)
+            if not os.path.isdir(full_name):
+                if full_name not in clientFileNames or clientFileNames[full_name]:
+                    new_file = new_files.add()
+                    new_file.file.filename = full_name
+                    new_file.file.isDirectory = False
+                    file_content = file_reader.file_read(full_name)
+                    new_file.fileContent = file_content
 
-
+"""
+This is the root parent method and is responsible for finding all the edited, deleted, and new files.
+"""
 def process_server_directory(clientRequest):
-     serverResponse = protocols_pb2.ServerResponse() 
-     serverResponse.timestamp = time.time()
-     clientFileNames = analyze_client_files(clientRequest.timestamp, clientRequest.clientFiles, serverResponse.deletedFiles, serverResponse.editedFiles)
-     analyze_server_files(clientRequest.directoryName, clientFileNames, serverResponse.newFiles)
-     return serverResponse
-
-
+    serverResponse = protocols_pb2.ServerResponse()
+    serverResponse.timestamp = time.time()
+    clientFileNames = analyze_client_files(
+        clientRequest.timestamp, clientRequest.clientFiles,
+        serverResponse.deletedFiles, serverResponse.editedFiles)
+    find_new_files(clientRequest.directoryName, clientFileNames,
+                         serverResponse.newFiles)
+    return serverResponse
